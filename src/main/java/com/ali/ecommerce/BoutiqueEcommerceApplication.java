3f5de380f59cb3f1690ee1c85b1de9a42e842284
Ali Essam Shaker <59261797+AliEssamShaker@users.noreply.github.com>
2021-06-17 15:53:05 -0600

    Add files via upload

---
+++ src/main/java/com/ali/ecommerce/BoutiqueEcommerceApplication.java
@@ -0,0 +1,13 @@
+package com.ali.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BoutiqueEcommerceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BoutiqueEcommerceApplication.class, args);
+	}
+
+}
